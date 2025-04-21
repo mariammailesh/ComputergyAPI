2a5db0945fee97178593b4272e273888b12c63e1
SuhaibAlkhaldi <suhaibamjad73@gmail.com>
2025-04-21 20:29:42 +0300

    Implement SignUp service

--- ComputergyAPI/DTOs/Authications/SignInInputDTO.cs
+++ ComputergyAPI/DTOs/Authications/SignInInputDTO.cs
@@ -2,8 +2,7 @@
 {
     public class SignInInputDTO
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        
         public string Email { get; set; }
         public string Password { get; set; }
     }
--- ComputergyAPI/Services/AuthanicationService.cs
+++ ComputergyAPI/Services/AuthanicationService.cs
@@ -1,10 +1,17 @@
-﻿using ComputergyAPI.DTOs.Authications;
+﻿using ComputergyAPI.Contexts;
+using ComputergyAPI.DTOs.Authications;
+using ComputergyAPI.Entites;
 using ComputergyAPI.Interfaces;
 
 namespace ComputergyAPI.Services
 {
     public class AuthanicationService : IAuthanication
     {
+        private readonly ComputergyDbContext _computergyDbContext;
+        public AuthanicationService(ComputergyDbContext computergyDbContext)
+        {
+            _computergyDbContext = computergyDbContext;
+        }
         public Task<bool> ResetPersonPassword(ResetPersonPasswordInputDTO input)
         {
             throw new NotImplementedException();
@@ -25,9 +32,18 @@ namespace ComputergyAPI.Services
             throw new NotImplementedException();
         }
 
-        public Task<string> SignUp(SignUpInputDTO input)
+        public async Task<string> SignUp(SignUpInputDTO input)
         {
-            throw new NotImplementedException();
+            Person person = new Person();
+            person.Email = input.Email;
+            person.Password = input.Password;
+            person.FirstName = input.FirstName;
+            person.LastName = input.LastName;
+            person.CreatedBy = "System";
+            person.CreationDate = DateTime.Now;
+            _computergyDbContext.Persons.Add(person);
+            _computergyDbContext.SaveChanges();
+            return "Account Created Successfully";
         }
     }
 }
