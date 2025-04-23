41fffc8c4e7bfd0a9f4841782abaa5277226421a
Ahmadziko <ahm00555@outlook.com>
2025-04-23 21:43:02 +0300

    Implement rating Entity and service

---
+++ ComputergyAPI/DTOs/Rating/CreateRateDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace ComputergyAPI.DTOs.Rating
+{
+    public class CreateRateDTO
+    {
+        public int PersonId { get; set; }
+        public int ItemId { get; set; }
+        public string Message { get; set; }
+        public int RateAmount { get; set; }
+    }
+}
