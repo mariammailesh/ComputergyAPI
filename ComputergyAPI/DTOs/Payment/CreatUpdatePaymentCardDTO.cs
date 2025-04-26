14318944cfce4cb3c98fd1fb9d3caf52cea10935
SuhaibAlkhaldi <suhaibamjad73@gmail.com>
2025-04-26 10:03:22 +0300

    Update CreateUpdatePaymentCardDTO

--- ComputergyAPI/DTOs/Payment/CreatUpdatePaymentCardDTO.cs
+++ ComputergyAPI/DTOs/Payment/CreatUpdatePaymentCardDTO.cs
@@ -2,11 +2,10 @@
 {
     public class CreateUpdatePaymentCardDTO
     {
-        public string? Status { get; set; }
         public string? CardNumber { get; set; }
         public string? CardHolder { get; set; }
         public string? ExpirationDate { get; set; }
         public string? CVC { get; set; }
-        public int UserId { get; set; }
+        public int? UserId { get; set; }
     }
 }
