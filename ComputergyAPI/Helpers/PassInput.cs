6c2c9f9c0a5ed31cf2e27bed33ac954ef21b5b08
Anas Al-Abdallat <Alabdallat-MAC-2014@DESKTOP-E8UDJO1>
2025-04-26 11:58:29 +0300

    Create Hash Password

---
+++ ComputergyAPI/Helpers/PassInput.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System.Security.Cryptography;
+
+namespace ComputergyAPI.Helpers
+{
+    public static class PasswordHasher
+    {
+        public static string HashPassword(string password)
+        {
+            byte[] salt = RandomNumberGenerator.GetBytes(16);
+
+            byte[] key = KeyDerivation.Pbkdf2(
+                password: password,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA256,
+                iterationCount: 100_000,
+                numBytesRequested: 32);
+
+            byte[] result = new byte[16 + 32];
+            Buffer.BlockCopy(salt, 0, result, 0, 16);
+            Buffer.BlockCopy(key, 0, result, 16, 32);
+
+            return Convert.ToBase64String(result);
+        }
+
+        public static bool VerifyPassword(string password, string hashedPassword)
+        {
+            byte[] decoded = Convert.FromBase64String(hashedPassword);
+
+            byte[] salt = new byte[16];
+            byte[] storedSubkey = new byte[32];
+            Buffer.BlockCopy(decoded, 0, salt, 0, 16);
+            Buffer.BlockCopy(decoded, 16, storedSubkey, 0, 32);
+
+            byte[] actualSubkey = KeyDerivation.Pbkdf2(
+                password: password,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA256,
+                iterationCount: 100_000,
+                numBytesRequested: 32);
+
+            return CryptographicOperations.FixedTimeEquals(storedSubkey, actualSubkey);
+        }
+    }
+}
