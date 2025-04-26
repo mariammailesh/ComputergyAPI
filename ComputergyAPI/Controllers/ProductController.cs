48b0014af74a54f397cef1d224dbf5ad1fac3c66
madaljeh <ahmadmadaljeh@gmail.com>
2025-04-26 11:43:20 +0300

    implement (Create&Update&delete) EndPoint

--- ComputergyAPI/Controllers/ProductController.cs
+++ ComputergyAPI/Controllers/ProductController.cs
@@ -2,6 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ComputergyAPI.Contexts;
+using ComputergyAPI.DTOs.Products;
+using ComputergyAPI.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ComputergyAPI.Controllers
@@ -10,6 +13,55 @@ namespace ComputergyAPI.Controllers
     [Route("api/[controller]")]
     public class ProductController : ControllerBase
     {
+        private readonly ProductsService _productsService;
 
+        public ProductController(ProductsService _productsService)
+        {
+            this._productsService = _productsService;
+        }
+
+
+        [HttpPost]
+        [Route("Create new Product")]
+        public async Task<IActionResult> CreateNewProduct(ProductCreateDTO dto)
+        {
+            try
+            {
+                return Ok( _productsService.CreateProduct(dto));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500,ex.Message);
+            }
+        }
+
+        [HttpPost]
+        [Route("Update Product")]
+
+        public async Task<IActionResult> UpdateProduct(ProductUpdateDTO dto)
+        {
+            try
+            {
+                return Ok(_productsService.UpdateProduct(dto));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpDelete]
+        [Route("Delete Product")]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            try
+            {
+                return Ok(_productsService.RemoveProduct(id));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
     }
 }
