537b65df16f435bc4742464c937c587b11ed4444
a.khader_tfs <a.khader_tfs@hotmail.com>
2025-04-26 12:25:57 +0300

    1

--- ComputergyAPI/DTOs/Payment/GetAllPaymentCardsDTO.cs
+++ ComputergyAPI/DTOs/Payment/GetAllPaymentCardsDTO.cs
@@ -5,6 +5,7 @@
         public string? CardNumber { get; set; }
         public string? CardHolder { get; set; }
         public string? ExpirationDate { get; set; }
+
         public string? CVC { get; set; }
         public int? UserId { get; set; }
     }
