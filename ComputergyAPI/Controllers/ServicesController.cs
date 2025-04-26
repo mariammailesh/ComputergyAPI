68ea851229598c90e696ef7e989f728619f5c35b
hi.yousef49@gmail.com <hi.yousef49@gmail.com>
2025-04-26 12:06:21 +0300

    Need to implement services controller

---
+++ ComputergyAPI/Controllers/ServicesController.cs
@@ -0,0 +1,19 @@
+﻿using ComputergyAPI.Contexts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ComputergyAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServicesController : ControllerBase
+    {
+        private readonly ComputergyDbContext _computergyDbContext;
+        public ServicesController(ComputergyDbContext computergyDbContext)
+        {
+            _computergyDbContext = computergyDbContext;
+        }
+
+        [Htt]
+    }
+}
--- ComputergyAPI/Interfaces/ICart.cs
+++ ComputergyAPI/Interfaces/ICart.cs
@@ -6,7 +6,7 @@ namespace ComputergyAPI.Interfaces
 {
     public interface  ICart
     {
-        Task<string> ClearCart(int cartId);
+        Task<string> ClearCart(int cartId); // 
         Task<string> CreateCart(int personId);
         Task<string> AddUpdateCart(CartItemDTO itemDTO);
         Task<string> RemoveFromCart(int CartitemId);
