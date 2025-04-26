7447087bd74c65e5a3f76f23dfd04778fd46d8ed
a.khader_tfs <a.khader_tfs@hotmail.com>
2025-04-26 12:09:49 +0300

    Implement Service Class for Payment services and Implement payment controller

--- ComputergyAPI/DTOs/Payment/CardDTO.cs
+++ ComputergyAPI/DTOs/Payment/CardDTO.cs
@@ -2,7 +2,7 @@
 
 namespace ComputergyAPI.DTOs.Payment
 {
-    public class CreateUpdatePaymentCardDTO
+    public class CardDTO: MainEntity
     {
         public string? CardNumber { get; set; }
         public string? CardHolder { get; set; }
