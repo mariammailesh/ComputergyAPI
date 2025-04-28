f0b1a511ebd564059a07699ec9ea99dae01e9c03
Malak Barakat <142212265+malak246@users.noreply.github.com>
2025-04-28 19:55:51 +0300

    Update Get Respons DTO & Services

--- ComputergyAPI/DTOs/Rating/Response/GetRateDTO.cs
+++ ComputergyAPI/DTOs/Rating/Response/GetRateDTO.cs
@@ -3,8 +3,9 @@
     public class GetRateDTO
     {
         public int PersonId { get; set; }
-        public string? Message { get; set; }
+        public string Message { get; set; }
         public int RateAmount { get; set; }
         public int OrderId { get; set; }
+        public DateTime OrderDate {get; set;}
     }
 }
