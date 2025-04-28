8ec163da24eac78a31e45f5e1addd66bf584f9d6
Jasser Alshaer <joshaer17@gmail.com>
2025-04-28 18:37:27 +0300

    Solve issue

---
+++ ComputergyAPI/DTOs/Products/SearchInputProductsDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace ComputergyAPI.DTOs.Products
+{
+    public class SearchInputProductsDTO
+    {
+        public string Name { get; set; }
+        public bool IsHasDiscount { get; set; }
+        public string Brand { get; set; }
+        public float ProductPrice { get; set; }
+        public string Category { get; set; }
+
+    }
+}
